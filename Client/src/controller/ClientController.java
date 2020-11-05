393a87962085b567534d8e92d7a554da10e93094
hquangit <61283175+hquangit@users.noreply.github.com>
2020-11-05 16:16:08 +0700

    Add files via upload

---
+++ Client/src/controller/ClientController.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import socketmodel.Login;
+import socketmodel.Register;
+import socketmodel.User;
+
+/**
+ *
+ * @author KyThuat88
+ */
+public class ClientController {
+
+    private Socket mySocket;
+    private String serverHost = "localhost";
+    private int serverPort = 8888;
+
+    public ClientController() {
+    }
+
+    public Socket openConnection() {
+        try {
+            mySocket = new Socket(serverHost, serverPort);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return mySocket;
+    }
+
+    public boolean sendData(Object ob) {
+        if(ob instanceof Login){
+            ob = (Login)ob;
+        }
+        
+        else if(ob instanceof Register){
+            ob = (Register) ob;
+        }
+        
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(mySocket.getOutputStream());
+            oos.writeObject(ob);
+
+        } catch (IOException ex) {
+            Logger.getLogger(ClientController.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+
+    public String receiveData() {
+        String result = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(mySocket.getInputStream());
+            Object o = ois.readObject();
+
+            if (o instanceof String) {
+                result = (String) o;
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(ClientController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClientController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return result;
+    }
+
+    public boolean closeConnection() {
+        try {
+            mySocket.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
